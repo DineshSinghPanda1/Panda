9ba042047cb8d6b755ec3c0ebe6bf721a9b39352
DineshSinghPanda1 <dineshpanda60@gmail.com>
2020-02-18 15:53:06 +0530

    new commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app>
+    <Syllabus />
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+//import Cards from "./components/Parent/Cards.vue";
+//import Marks from "./components/Parent/Marks.vue";
+//import Mesh from "./components/Parent/Mesh.vue";
+//import Marksheet from "./components/Parent/Marksheet.vue";
+//import TCReq from "./components/Parent/TCReq.vue";
+//import Navbar from "./components/Parent/Navbar.vue";
+//import Appointment from "./components/Parent/Appointment.vue";
+
+import Syllabus from "./components/Teacher/Syllabus.vue";
+//import Application from "./components/Teacher/Application.vue";
+//import Recommend from "./components/Teacher/Recommend.vue";
+
+export default Vue.extend({
+  name: "App",
+
+  components: {
+    //Cards
+    //Marks
+    //Marksheet
+    //Mesh
+    //Marksheet,
+    //TCReq
+    // Appointment
+    //Application
+    //Navbar
+    //Recommend
+    Syllabus
+  },
+
+  data: () => ({
+    //
+  })
+});
+</script>
